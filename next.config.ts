d8cff1aef8e4fab410747cf8962ac9ec1ebaa0b9
safr-labs <safrlabs@gmail.com>
2026-09-20 21:59:48 +0530

    added

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,7 @@ const withPWA = withPWAInit({
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['172.24.204.216', 'localhost', '127.0.0.1'],
 };
 
 export default withPWA(nextConfig);
